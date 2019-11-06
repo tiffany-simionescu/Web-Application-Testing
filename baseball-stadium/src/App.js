ac2b0677cb9c5a656cfba5aa54ac6380f028fea8
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-11-05 20:10:35 -0500

    Working on the functionality of components

--- baseball-stadium/src/App.js
+++ baseball-stadium/src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      strikes: 0,
+      balls: 0,
+      fouls: 0,
+      hits: 0
+    }
+  }
+
+    handleStrike = () => {
+
+    }
+
+    handleBall = () => {
+
+    }
+
+    handleFoul = () => {
+
+    }
+
+    handleHit = () => {
+
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <Display />
+        <Dashboard />
+      </div>
+    )
+  }
 }
 
 export default App;

da5e294e389e00ad8c685fbe7f7261e817042c5f
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-11-05 21:10:22 -0500

    Finished the handle events

--- baseball-stadium/src/App.js
+++ baseball-stadium/src/App.js
@@ -15,26 +15,54 @@ class App extends React.Component {
   }
 
     handleStrike = () => {
-
+      if(this.state.strikes > 2) {
+        this.setState({
+          strikes: 0
+        })
+      } else {
+        this.setState({
+          strikes: this.state.strikes + 1
+        })
+      } 
     }
 
     handleBall = () => {
-
+      if(this.state.balls > 3) {
+        this.setState({
+          balls: 0
+        })
+      } else {
+        this.setState({
+          balls: this.state.balls + 1
+        })
+      } 
     }
 
     handleFoul = () => {
-
+      if (this.state.strikes < 2) {
+        this.setState({
+          strikes: this.state.strikes + 1
+        })
+      }
     }
 
     handleHit = () => {
-
+      this.setState({
+        strikes: 0,
+        balls: 0
+      })
     }
 
   render() {
     return (
       <div className="App">
-        <Display />
-        <Dashboard />
+        <Display score={this.state} />
+        <Dashboard 
+          handleStrike={this.handleStrike} 
+          handleBall={this.handleBall} 
+          handleFoul={this.handleFoul}
+          handleHit={this.handleHit}
+        />
       </div>
     )
   }
--- baseball-stadium/src/components/Display.js
+++ baseball-stadium/src/components/Display.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Display(props) {
   return (
     <div>
-      <h3>Strikes: {props.strikes}</h3>
-      <h3>Balls: {props.balls}</h3>
+      <h3>Strikes: {props.score.strikes}</h3>
+      <h3>Balls: {props.score.balls}</h3>
     </div>
   );
 }
